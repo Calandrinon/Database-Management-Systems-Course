3bdb9c03b42baa9c80fb3d0e49d72a7f05fce352
Calandrinon <georgesut@yahoo.com>
2021-03-01 16:56:28 +0200

    Add a Winforms/ADO.NET sample
    This is built on top of the database designed last semester

---
+++ Lab-1/WinForms-ADOdotNet-Sample/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Send = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(351, 296);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(94, 29);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Get artists";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(351, 27);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(50, 20);
+            this.message.TabIndex = 1;
+            this.message.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.Send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Label message;
+    }
+}
+
---
+++ Lab-1/WinForms-ADOdotNet-Sample/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            message.Text = "";
+            String connectionString = "Data Source=DESKTOP-KPQA7E3\\SQLEXPRESS; Initial Catalog=OnlineMusicStore; Integrated Security=true";
+            DataSet ds = new DataSet();
+
+            using (SqlConnection dbConn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Artist", dbConn);
+                dbConn.Open();
+                da.Fill(ds, "Artist");
+                DataTable dt = ds.Tables["Artist"];
+                foreach (DataRow row in dt.Rows)
+                {
+                    message.Text += row["Name"] + "\n";
+                }
+            }
+        }
+    }
+}
