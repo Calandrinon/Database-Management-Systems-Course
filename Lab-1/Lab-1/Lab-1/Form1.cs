8c008ab0e9a81532457a371f47dcf361e615bc70
Calandrinon <georgesut@yahoo.com>
2021-03-02 09:21:22 +0200

    Add rows from the ClientUser table to a list view

--- Lab-1/Lab-1/Lab-1/Form1.cs
+++ Lab-1/Lab-1/Lab-1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,39 @@ namespace Lab_1
 {
     public partial class Form1 : Form
     {
+        private SqlConnection databaseConnection;
         public Form1()
         {
             InitializeComponent();
+            String connectionString = "Data Source=DESKTOP-KPQA7E3\\SQLEXPRESS; Initial Catalog=OnlineMusicStore; Integrated Security=true";
+            this.databaseConnection = new SqlConnection(connectionString);
         }
 
         private void displayUsersButton_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("hello world");
-            String[] row = {"some id", "some other stuff", "some more", "less"};
-            ListViewItem listViewItem = new ListViewItem(row);
-            usersListView.Items.Add(listViewItem);
+            usersListView.Items.Clear();
+
+            databaseConnection.Open();
+            DataSet dataSet = new DataSet();
+            String query = "SELECT * FROM ClientUser";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, databaseConnection);
+            dataAdapter.Fill(dataSet, "ClientUser");
+            DataTable dataTable = dataSet.Tables["ClientUser"];
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {                        
+                String[] rowStringArray = {dataRow["UserId"].ToString(),
+                    dataRow["Username"].ToString(),
+                    dataRow["EncryptedPassword"].ToString(),
+                    dataRow["RegistrationDate"].ToString()};
+                ListViewItem listViewItem = new ListViewItem(rowStringArray);
+
+                usersListView.Items.Add(listViewItem);
+
+                Console.WriteLine(dataRow["UserId"] + " " + dataRow["Username"].ToString() + " " + dataRow["EncryptedPassword"] + " " + dataRow["RegistrationDate"]);
+            }
+
+            databaseConnection.Close();
         }
     }
 }
